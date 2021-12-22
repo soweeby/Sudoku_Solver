aae7b946efa0e345195be217aa8ae3339d0e73b9
soweeby <rgnunna@gmail.com>
2021-12-22 17:35:06 -0500

    added methods for getting neighbor permutations, and checking to make sure all rows and columns are valid

--- src/Sudoku_Puzzle/SudokuInstance.java
+++ src/Sudoku_Puzzle/SudokuInstance.java
@@ -5,12 +5,19 @@
 package Sudoku_Puzzle;
 
 import java.io.*;
+import java.util.*;
 
 public class SudokuInstance {
     private int rows;
     private int cols;
+    private int cursor = -1; //cursor starts off the board
     private int[][] board; //creates a 9x9 board - typical sudoku board
 
+    /**
+     * Creates a new SudokuInstance by reading from a file.
+     * @param filename filename of file to read
+     * @throws IOException
+     */
     public SudokuInstance(String filename) throws IOException {
         try {
             BufferedReader in = new BufferedReader(new FileReader(filename));
@@ -33,6 +40,70 @@ public class SudokuInstance {
         }
     }
 
+    private SudokuInstance(SudokuInstance other, int num) {
+        this.cursor = other.cursor + 1;
+        this.rows = other.rows;
+        this.cols = other.cols;
+        this.board = new int[this.rows][this.cols];
+        for (int crow = 0; crow < this.rows; ++crow) {
+            System.arraycopy(other.board[crow], 0, this.board[crow], 0, this.cols);
+        }
+
+        int row = this.cursor / this.rows;
+        int col = this.cursor % this.cols;
+
+        this.board[row][col] = num;
+    }
+
+    /**
+     * Gets permutations of a particular SudokuInstance 1 spot over to the right
+     * @return list of permutations
+     */
+    public List<SudokuInstance> getPermutations() {
+        ArrayList<SudokuInstance> permutations = new ArrayList<>();
+        for (int i = 1; i < this.rows; ++i) {
+            permutations.add(new SudokuInstance(this, i));
+        }
+        return permutations;
+    }
+
+    /**
+     * makes sure there are no duplicates in the rows
+     * @return true if no duplicates, false if there are duplicates
+     */
+    public boolean validRows() {
+        HashSet<Integer> ints = new HashSet<>();
+        for (int i = 0; i < this.rows; ++i) {
+            for (int j = 0; j < this.cols; ++j) {
+                if (ints.contains(this.board[i][j])) {
+                    return false;
+                }
+                if (this.board[i][j] != 0) {
+                    ints.add(this.board[i][j]);
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * make sure there are no duplicates in the columns
+     * @return true if no duplicates, false if there are duplicates
+     */
+    public boolean validCols() {
+        HashSet<Integer> ints = new HashSet<>();
+        for (int i = 0; i < this.cols; ++i) {
+            for (int j = 0; j < this.rows; ++j) {
+                if (ints.contains(this.board[j][i])) {
+                    return false;
+                }
+                if (this.board[j][i] != 0) {
+                    ints.add(this.board[j][i]);
+                }
+            }
+        }
+        return true;
+    }
     @Override
     public String toString() {
         String finString = "";
@@ -44,5 +115,20 @@ public class SudokuInstance {
             finString += "\n";
         }
         return finString;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SudokuInstance that = (SudokuInstance) o;
+        return rows == that.rows && cols == that.cols && Arrays.equals(board, that.board);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(rows, cols);
+        result = 31 * result + Arrays.hashCode(board);
+        return result;
     }
 }
