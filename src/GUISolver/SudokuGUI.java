ff2373501888bd8322b929988f125f38db29d314
soweeby <rgnunna@gmail.com>
2021-12-22 19:47:36 -0500

    Added init for SudokuGUI and started implementing SudokuModel

--- src/GUISolver/SudokuGUI.java
+++ src/GUISolver/SudokuGUI.java
@@ -3,7 +3,22 @@ package GUISolver;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class SudokuGUI extends Application {
+    private SudokuModel model;
+
+    public void init() throws IOException {
+        try {
+            String filename = getParameters().getRaw().get(0);
+        }
+        catch (NullPointerException e) {
+            String filename = null;
+        }
+
+        this.model.addGUI(this);
+    }
+
 
     public static void main(String[] args) {
         launch(args);
--- src/GUISolver/SudokuModel.java
+++ src/GUISolver/SudokuModel.java
@@ -1,4 +1,30 @@
+/**
+ * @author Roshan Nunna
+ * Model for GUI Solver
+ */
 package GUISolver;
 
+import Sudoku_Puzzle.SudokuInstance;
+
+import java.io.IOException;
+
 public class SudokuModel {
+    private SudokuGUI gui;
+    private SudokuInstance puzzle;
+
+    public SudokuModel() {
+
+    }
+
+    public void addGUI(SudokuGUI gui) {
+        this.gui = gui;
+    }
+
+    public void updateGUI(String data) {
+
+    }
+
+    public void load(String filename) throws IOException {
+        this.puzzle = new SudokuInstance(filename);
+    }
 }
